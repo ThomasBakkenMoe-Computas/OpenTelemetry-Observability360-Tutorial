a8add2b10bcbf2e6632172999268507632bc3b28
Thomas Bakken Moe <thom@computas.com>
2024-09-04 13:17:21 +0200

    add otel builder config

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+const string serviceName = "relay-service";
+const string serviceVersion = "1.0.0";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options.SetResourceBuilder(
+        ResourceBuilder.CreateDefault()
+            .AddService(serviceName, serviceVersion))
+        .AddConsoleExporter();
+});
 
 var app = builder.Build();
 
